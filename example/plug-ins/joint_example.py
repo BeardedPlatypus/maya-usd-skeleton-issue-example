3e5a559db413fe608ebc41fb86b0864a645c0f00
BeardedPlatypus <mwtegelaers@gmail.com>
2022-11-09 20:51:03 -0500

    Fix mistakes joint_example.py

--- example/plug-ins/joint_example.py
+++ example/plug-ins/joint_example.py
@@ -1,4 +1,5 @@
 import maya
+import maya.cmds as cmds
 import maya.api.OpenMaya as api
 import mayaUsd.lib
 
@@ -6,40 +7,45 @@ import mayaUsd.lib
 def maya_useNewApi():
     pass
 
-
 class JointAdaptor(mayaUsd.lib.SchemaApiAdaptor):
     @property
     def maya_object_name(self):
         return api.MFnDependencyNode(self.mayaObject).name()
 
-    def print(self, msg):
-        print(f"{self.__name__}: {msg}")
-
     def CanAdaptForImport(self, jobArgs):
-        self.print(f"CanAdaptForImport: {self.maya_object_name}")
+        print(f"CanAdaptForImport: {self.maya_object_name}")
         return True
 
     def ApplySchemaForImport(self, primReaderArgs, context):
-        self.print(f"ApplySchemaForImport: {self.maya_object_name}")
+        print(f"ApplySchemaForImport: {self.maya_object_name}")
         return True
 
     def CopyFromPrim(self, prim, args, context):
         value = prim.GetAttribute("example:data").Get()
-        self.print(f"CopyFromPrim: mayaObject: {self.maya_object_name} | prim: {prim.GetName()} | value: {value}")
+        print(f"CopyFromPrim: mayaObject: {self.maya_object_name} | prim: {prim.GetName()} | value: {value}")
+
+        # Add a simple boolean to indicate we imported data on it.
+        cmds.select(self.maya_object_name)
+        cmds.addAttr(longName="with_adaptor", attributeType="bool")
+        attr = "{}.{}".format(self.maya_object_name, "with_adaptor")
+        cmds.setAttr(attr, True)
         return True
 
     def CanAdaptForExport(self, jobArgs):
-        self.print(f"CanAdaptForExport: {self.maya_object_name}")
+        if not cmds.attributeQuery("with_adaptor", node=self.maya_object_name, exists=True):
+            return False
+
+        print(f"CanAdaptForExport: {self.maya_object_name}")
         return True
 
     def CopyToPrim(self, prim, usdTime, valueWriter):
-        self.print(f"CopyToPrim: mayaObject: {self.maya_object_name} | prim: {prim.GetName()}")
-        value = prim.GetAttribute("example:data").Set("this-was-exported")
+        print(f"CopyToPrim: mayaObject: {self.maya_object_name} | prim: {prim.GetName()}")
+        prim.GetAttribute("example:data").Set("this-was-exported")
         return True
 
-    
+
 def initializePlugin(object):
-    if not maya.cmds.plugInfo("mayaUsdPlugin", query=True, loaded=True):
+    if not maya.cmds.pluginInfo("mayaUsdPlugin", query=True, loaded=True):
         maya.cmds.loadPlugin("mayaUsdPlugin")
 
     mayaUsd.lib.SchemaApiAdaptor.Register(JointAdaptor, "entity", "WithAdaptorAPI")
